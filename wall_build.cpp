c79cfa62f974fae11861fc89cfc3b820063ad3a9
kiseliov <dmitrykiseliov2000@gmail.com>
2018-12-06 02:03:50 +0300

    well done sigmentation fault!

--- wall_build.cpp
+++ wall_build.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream>
+#include <cstring>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -7,7 +9,7 @@
 using namespace std;
 
 const int THE_END = -1;
-long widht = 5;
+long width = 5;
 long height = 2;
 long min_son = 2;
 long max_son = 4;
@@ -24,24 +26,24 @@ void show_wall() {
 	cout << endl;
 }
 
-void* father(void* arg) {
+void *father_func(void *arg) {
 	long x = THE_END, y;
 	for (;;) {
 		pthread_mutex_lock(&mutex);
 		if (x >= 0) {
 			their_wall[x] = their_wall[x] + 'F';
-			print_their_wall();
+			show_wall();
 		}
 		x = THE_END;
-		for (long m = 1; m =< height; m += 2) {
+		for (long m = 1; m <= height; m += 2) {
 			for (long n = 0; ; ++n) {
 				long temp;
-				temp = (m)%2 + n*2
-				if (temp > width*2)
+				temp = (m)%2 + n*2;
+				if (temp > width*2){
 					break;
-				else if (their_wall[m][temp] != ' ')
+				}else if (their_wall[m][temp] != ' '){
 					continue; 
-				else if ((their_wall[m-1][temp] != ' ') && (their_wall[m-1][temp+1] != ' ')) {
+				}else if ((their_wall[m-1][temp] != ' ') && (their_wall[m-1][temp+1] != ' ')) {
 					x = m;
 					y = temp;
 					m = height;
@@ -65,21 +67,22 @@ void* father(void* arg) {
 }
 
 
-void* son(void* arg) {
+void *son_func(void *arg) {
 	long x = THE_END, y;
 	for (;;) {
 		pthread_mutex_lock(&mutex);
 		if (x >= 0) {
 			their_wall[x] = their_wall[x] + 'S';
-			print_their_wall();
+			show_wall();
 		}
 		x = THE_END;
-		for (long m = 2; m =< height; m += 2) {
+		for (long m = 2; m <= height; m += 2) {
 			for (long n = 0; ; ++n) {
 				long temp;
-				temp = m%2 + n*2
-				if (temp > width*2)
+				temp = m%2 + n*2;
+				if (temp > width*2){
 					break;
+				}
 				else if (their_wall[m][temp] != ' ')
 					continue; 
 				else if ((their_wall[m-1][temp] != ' ') && (their_wall[m-1][temp+1] != ' ')) {
@@ -104,25 +107,25 @@ void* son(void* arg) {
 	}
 }
 
-int main{
-	sscanf("%d %d \n %d %d \n %d %d", &widht, &height, &min_father, &max_father, &min_son, &max_son);
-	their_wall = (string*) malloc((Height+1) * sizeof(string));
-	for (size_t i = 0; i <= Height; ++i) {
-		for (size_t j = 0; j <= 2*Width+1; ++j)
+int main(){
+	scanf("%ld %ld \n %ld %ld \n %ld %ld", &width, &height, &min_father, &max_father, &min_son, &max_son);
+	their_wall = (string*) malloc((height+1) * sizeof(string));
+	for (long i = 0; i <= height; ++i) {
+		for (long j = 0; j <= 2*width+1; ++j)
 			their_wall[i][j] = ' ';
-		their_wall[i][(Width+1)*2+1] = 0;
+		their_wall[i][(width+1)*2+1] = 0;
 	}
-	for (size_t j = 0; j <= 2*Width+1; ++j)
+	for (long j = 0; j <= 2*width+1; ++j)
 		their_wall[0][j] = '#';
-	for (size_t i = 1; i <= Height; i += 2) {
+	for (long i = 1; i <= height; i += 2) {
 		their_wall[i][0] = '#';
-		their_wall[i][2*Width+1] = '#';
+		their_wall[i][2*width+1] = '#';
 	}
 	show_wall();
 	pthread_mutex_init(&mutex, NULL);
 	pthread_t son, father;
-	pthread_create(&father, NULL, father, NULL);
-	pthread_create(&son, NULL, son, NULL);
+	pthread_create(&father, NULL, father_func, NULL);
+	pthread_create(&son, NULL, son_func, NULL);
 	pthread_join(son, NULL);
 	pthread_join(father, NULL);
 }
